f4b5c71b2066d2da22137c888fd60310e30fcdd3
sheikhug <sheikhug@users.noreply.github.com>
2024-12-27 02:07:27 +1100

    initional commit of complete cdk code for Big-data analysis architecture.4

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,14 +7,12 @@ import { aws_glue as glue2 } from 'aws-cdk-lib';
 import { aws_iam as iam } from 'aws-cdk-lib';
 import { aws_athena as athena } from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-// import * as s3deploy from '@aws-cdk/aws-s3-deployment';
 import { aws_s3_deployment as s3deploy } from 'aws-cdk-lib';
 import { aws_kinesisfirehose as firehose } from 'aws-cdk-lib';
 import * as lambda_ from 'aws-cdk-lib/aws-lambda';
 import * as events from 'aws-cdk-lib/aws-events';
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as quicksight from 'aws-cdk-lib/aws-quicksight';
-
 import { aws_ec2 as ec2 } from 'aws-cdk-lib'
 import * as emr from 'aws-cdk-lib/aws-emr';
 
@@ -368,6 +366,152 @@ def handler(event, context):
     new cdk.CfnOutput(this, 'Data_stream_bucket',       {value: dataStream.s3Bucket?.bucketName || ''});
     new cdk.CfnOutput(this, 'Glue_database_Name',       {value: glueDatabase.databaseName});
     new cdk.CfnOutput(this, 'AthenaResultsBucketName',  {value: athenaOutputBucket.bucketName,});
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+    //     // Add a bucket policy for QuickSight to access the S3 bucket
+    //     const quickSightArn = `arn:aws:quicksight:${this.region}:${this.account}:namespace/my-custom-namespace`;
+    //     const quickSightRoleArn = `arn:aws:iam::${this.account}:role/service-role/aws-quicksight-service-role-v0`;
+
+    //     const quickSightServiceRole = new iam.Role(this, 'QuickSightServiceRole', {
+    //       assumedBy: new iam.ServicePrincipal('quicksight.amazonaws.com'),
+    //       managedPolicies: [
+    //         iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSQuickSightAthenaAccess'),
+    //       ],
+    //     });
+
+
+    //     athenaOutputBucket.addToResourcePolicy(
+    //       new iam.PolicyStatement({
+    //         effect: iam.Effect.ALLOW,
+    //         principals: [new iam.ServicePrincipal('quicksight.amazonaws.com')],
+    //         actions: ['s3:GetObject', 's3:ListBucket'],
+    //         resources: [athenaOutputBucket.bucketArn, `${athenaOutputBucket.bucketArn}/*`],
+    //       })
+    //     );
+
+
+
+
+    //     // Create QuickSight Athena Data Source
+    //     const athenaDataSource = new quicksight.CfnDataSource(this, 'AthenaDataSource', {
+    //       awsAccountId: this.account,
+    //       dataSourceId: 'AthenaDataSource',
+    //       name: 'AthenaDataSource',
+    //       type: 'ATHENA',
+    //       dataSourceParameters: {
+    //         athenaParameters: {
+    //           workGroup: athenaWorkgroup.name,
+    //         },
+    //       },
+    //       permissions: [
+    //         {
+    //           principal: quickSightServiceRole.roleArn,
+    //           actions: ['quicksight:DescribeDataSource', 'quicksight:UpdateDataSourcePermissions'],
+    //         },
+    //       ],
+    //     });
+
+
+    //     // Lambda function to automate QuickSight dataset creation (Python runtime)
+    //     const quickSightDatasetLambda = new lambda_.Function(this, 'QuickSightDatasetLambda', {
+    //       runtime: lambda_.Runtime.PYTHON_3_9,
+    //       code: lambda_.Code.fromInline(`
+    // import boto3
+    // import os
+
+
+    // def convert_glue_type_to_typescript(glue_type):
+    //     type_mapping = {
+    //         'string': 'STRING',
+    //         'int': 'INTEGER',
+    //         'bigint': 'INTEGER',
+    //         'float': 'DECIMAL',
+    //         'double': 'DECIMAL',
+    //         'boolean': 'BOOLEAN',
+    //         'date': 'DATETIME',
+    //         'timestamp': 'DATETIME'
+    //     }
+    //     return type_mapping.get(glue_type, 'STRING')  # Default to STRING if type is unknown
+
+
+    // def get_quicksight_input_columns(database_name, table_name):
+    //     glue_client = boto3.client('glue')
+    //     response = glue_client.get_table(DatabaseName=database_name, Name=table_name)
+    //     columns = response['Table']['StorageDescriptor']['Columns']
+    //     return [
+    //         {'Name': col['Name'], 'Type': convert_glue_type_to_typescript(col['Type'])}
+    //         for col in columns
+    //     ]
+
+    // # Example usage
+    // database_name = 'example-database'
+    // table_name = 'example-table'
+    // columns = get_quicksight_input_columns(os.environ['DATABASE_NAME'], os.environ['TABLE_NAME'])
+
+    // print(columns)
+
+
+    // def handler(event, context):
+    //     quicksight = boto3.client('quicksight')
+
+    //     params = {
+    //         'AwsAccountId': os.environ['ACCOUNT_ID'],
+    //         'DataSetId': 'AthenaDataSet',
+    //         'Name': 'AthenaDataSet',
+    //         'ImportMode': 'SPICE',
+    //         'PhysicalTableMap': {
+    //             'PhysicalTableId': {
+    //                 'S3Source': {
+    //                     'DataSourceArn': os.environ['DATASOURCE_ARN'],
+    //                     'InputColumns': columns,
+    //                     'UploadSettings': {
+    //                         'Format': 'CSV',
+    //                         'StartFromRow': 1,
+    //                         'ContainsHeader': True
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     response = quicksight.create_data_set(**params)
+    //     print('Dataset created:', response)
+
+    //       `),
+    //       handler: 'index.handler',
+    //       environment: {
+    //         ACCOUNT_ID: this.account,
+    //         DATABASE_NAME: glueDatabase.databaseName,
+    //         TABLE_NAME: stream_bucket_name,
+    //         // AWS_REGION: this.region,
+    //         DATASOURCE_ARN: `arn:aws:quicksight:${this.region}:${this.account}:datasource/AthenaDataSource`,
+    //       },
+    //     });
+
+    //     quickSightDatasetLambda.addToRolePolicy(
+    //       new iam.PolicyStatement({
+    //         effect: iam.Effect.ALLOW,
+    //         actions: ['quicksight:CreateDataSet', 'quicksight:UpdateDataSet', 'glue:GetTable'],
+    //         resources: ['*'],
+    //       })
+    //     );
+
+
 
 
   }
